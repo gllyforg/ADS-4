8081a11a3aed6d43741661fc2503089282a7f48e
gllyforg <shilovaksenia2018@mail.ru>
2026-04-05 19:15:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,87 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;    
+    for (int i = 0; i < len - 1; i++) {
+        int target = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        int firstPos = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                firstPos = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        if (firstPos != -1) {
+            left = firstPos;
+            right = len - 1;
+            int lastPos = firstPos;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (arr[mid] == target) {
+                    lastPos = mid;
+                    left = mid + 1;
+                } else if (arr[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            
+            count += (lastPos - firstPos + 1);
+        }
+    }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];        
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                // Если элементы равны, то количество пар = C(k,2), где k = right - left + 1
+                int k = right - left + 1;
+                count += k * (k - 1) / 2;
+                break;
+            } else {
+                int leftVal = arr[left];
+                int leftCount = 0;
+                while (left < right && arr[left] == leftVal) {
+                    leftCount++;
+                    left++;
+                }
+                int rightVal = arr[right];
+                int rightCount = 0;
+                while (left <= right && arr[right] == rightVal) {
+                    rightCount++;
+                    right--;
+                }
+                count += leftCount * rightCount;
+            }
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+  return count;
 }
